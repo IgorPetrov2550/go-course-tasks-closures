bad2e4ddf9dd9605971a5a825fb87ef74601d1ef
IgorPetrov2550 <Igorpetrov2550@gmail.com>
2026-04-28 23:24:56 +0500

    Task01

--- task01/main.go
+++ task01/main.go
@@ -28,15 +28,22 @@ package main
 import "fmt"
 
 // TODO: напиши функцию makeMultiplier(factor int) func(int) int
+
+func makeMultiplier(factor int) func(int) int {
+	return func(n int) int { return n * factor }
+}
+
 // Подсказка: внутри верни func(n int) int { return n * factor }
 // factor - это и есть то, что "захватывает" замыкание
 
 func main() {
 	// TODO: создай double := makeMultiplier(2) и triple := makeMultiplier(3)
-
+	double := makeMultiplier(2)
+	triple := makeMultiplier(3)
 	// TODO: вызови double(5), double(7), triple(5), triple(7) и выведи результаты
-
+	fmt.Println("double(5) = ", double(5))
+	fmt.Println("double(7) = ", double(7))
+	fmt.Println("triple(5) = ", triple(5))
+	fmt.Println("triple(7) = ", triple(7))
 	// TODO: добавь fmt.Println("Это одна и та же фабрика, но разные замыкания!")
-
-	_ = fmt.Println // убери когда начнёшь использовать
 }
